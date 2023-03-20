fd6556e28ec81d5d474f90471527c189a9e1ef48
Peyton Howard <hpeyton12@outlook.com>
2023-03-20 16:54:15 -0400

    working on drawing the lines

--- project_2.js
+++ project_2.js
@@ -5,13 +5,18 @@ var colorpick = 0;
 var colors = [
     vec4(1.0, 0.0, 0.0, 1.0), // red
     vec4(0.0, 0.0, 1.0, 1.0), // blue
-    vec4(1.0, 1.0, 0.0, 1.0), //yellow
+    vec4(1.0, 1.0, 0.0, 1.0) //yellow
 ]; // array of colors
+
 var c = [];
 var vgrid = new Float32Array([-0.6,-1 , -0.6,1 , -0.2,-1 , -0.2,1 , 0.2,-1 , 0.2,1 , 0.6,-1 , 0.6,1]);
 
-function drawgrid(){
-
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas"); // get link to canvas
+    gl = canvas.getContext("webgl2"); // get context for drawings
+    if (!gl) { alert("WebGL 2.0 isn't available"); }
+    
+    colorpick = 2;
     c.push(colors[colorpick]);
 
     // configure webgl - intialize our drawing
@@ -24,7 +29,7 @@ function drawgrid(){
     // load data into GPU
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId); // What comes next should affect bufferId
-    gl.bufferData(gl.ARRAY_BUFFER, vgrid, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vgrid), gl.STATIC_DRAW);
 
     // tie aPosition to the data in the buffer
     var aPosition = gl.getAttribLocation(program, "aPosition"); // connect to variable in shader
@@ -36,22 +41,12 @@ function drawgrid(){
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferColorId); // what comes next should affect bufferId
     gl.bufferData(gl.ARRAY_BUFFER, flatten(c), gl.STATIC_DRAW);
 
+    //tie aColor to the data in the buffer
     var aColor = gl.getAttribLocation(program, "aColor");
     gl.vertexAttribPointer(aColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aColor);
 
     gl.clear(gl.COLOR_BUFFER_BIT); // completely clear color
-    gl.drawArrays(gl.Lines, 0, 8); // renders a triangle with 3 points
-}
+    gl.drawArrays(gl.LINES, 0, 4); // renders a triangle with 3 points
 
-window.onload = function init() {
-    canvas = document.getElementById("gl-canvas"); // get link to canvas
-    gl = canvas.getContext("webgl2"); // get context for drawings
-    if (!gl) { alert("WebGL 2.0 isn't available"); }
-    drawgrid;
-
-    function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, positions.length);
-    }
 }
